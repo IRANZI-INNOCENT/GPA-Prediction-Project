ceb38d03bab2c99b8ad6965cc21989bfc6b3f511
John Doe <IRANZI-INNOCENT>
2024-03-09 00:02:51 +0300

    First Preoject

---
+++ GPA_app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import numpy as np
+from GPA_prediction import predict
+
+st.title('GPA PREDICTION APP')
+
+st.markdown('Enter your preferred parameters to predict your GPA. For Science,Engineering, Arts, Business and jobs; Enter 1 for Yes and 0 for NO')
+
+st.header("STUDENTS STUDY PATTERNS")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.text("STUDY TIME")
+    attendance = st.slider('Attendance', 0, 100, 5)
+    class_hours = st.slider('Class_hours_week', 0, 25, 1)
+    online_learning = st.slider('Online_learning', 0, 25, 1)
+    library_hours = st.slider('Library_hours', 0, 25, 1)
+
+with col2:
+    st.text("ENGAGEMENTS")
+    jobs = st.slider('jobs', 0, 1, 0)
+    extracurricular = st.slider('Extracurricular', 0, 10, 0)
+    sleep_hours = st.slider('Sleep_hours', 0, 10, 0)
+
+with col3:
+    st.text("MAJOR")
+    business = st.slider('Business', 0, 1, 0)
+    science = st.slider('Science', 0, 1, 0)
+    engineering = st.slider('Engineering', 0, 1, 0)
+    arts = st.slider('Arts', 0, 1, 0)
+
+st.text('')
+if st.button("Predict GPA"):
+    # Call predict function
+    result = predict(np.array([[attendance, class_hours, online_learning, jobs, extracurricular, sleep_hours, library_hours, arts,business, science, engineering]]))
+    st.text('Predicted GPA: {:.2f}'.format(result[0]))
+
+st.text('')
+st.text('')
+st.markdown(
+    '`By`  IRANZI ft MARGRET | ` Code:` [GitHub](https://github.com/IRANZI-INNOCENT/Data-Sciennce)')
---
+++ GPA_model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+import joblib
+
+Datathon = pd.read_csv('data.csv')
+x,y = Datathon[['Class_hours_week','Attendance','Extracurricular','Online_learning','Sleep_hours','Business','Library_hours','Arts',
+               'Engineering','Science','jobs']].values, Datathon["College GPA"].values
+x_train, x_test,y_train,y_test = train_test_split(x,y, test_size=0.3, random_state=0)
+model = LinearRegression().fit(x_train, y_train)
+
+
+predictions = model.predict(x_test)
+mse = mean_squared_error(y_test, predictions)
+print('Mean Squared Error:', mse)
+joblib.dump(model, 'gpa_prediction_model.pkl')
