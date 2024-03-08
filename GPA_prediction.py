693c072b462cd0d6abbb9b8c6219a380d93caae3
IRANZI-INNOCENT <160141125+IRANZI-INNOCENT@users.noreply.github.com>
2024-03-09 00:54:38 +0300

    Update GPA_prediction.py
    Update file path in GPA_prediction.py

--- GPA_prediction.py
+++ GPA_prediction.py
@@ -1,6 +1,6 @@
 import joblib
 
 def predict(data):
-    gpa_model = joblib.load("C:\\Users\\Admin\\OneDrive\\Desktop\\ML PROJECT\\GPA App\\gpa_prediction_model.pkl")
+    gpa_model = joblib.load("gpa_prediction_model.pkl")
     predicted_gpa = gpa_model.predict(data)
     return predicted_gpa
